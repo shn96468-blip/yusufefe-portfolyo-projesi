c1b61fe894173b4a2ed19fabd3f1a36bea63543d
shn96468-blip <shn96468@gmail.com>
2025-11-12 16:29:43 +0300

    Update app_final.py

--- app_final.py
+++ app_final.py
@@ -1,19 +1,6 @@
-# --- SES KONTROLLERİ (Ana Sayfa) ---
-    col_kapat, col_ac, col_volume_slider = st.columns([1, 1, 6]) 
+import streamlit as st
+import time
 
-    if st.session_state['music_enabled']:
-        with col_kapat:
-            if st.button("🔊 Kapat", key="btn_kapat_ses", use_container_width=True):
-                st.session_state['music_enabled'] = False
-                st.rerun()
-        with col_volume_slider:
-            # Ses seviyesi kaydırıcısı
-            new_volume = st.slider("Ses Seviyesi", 0.0, 1.0, st.session_state['music_volume'], step=0.1, key="music_volume_slider")
-            if new_volume != st.session_state['music_volume']:
-                st.session_state['music_volume'] = new_volume
-                st.rerun()
-    elif st.session_state['music_url']: 
-        with col_ac:
-            if st.button("🔇 Aç", key="btn_ac_ses", use_container_width=True):
-                st.session_state['music_enabled'] = True
-                st.rerun()
+# --- OTURUM DURUMU (SESSION STATE) BAŞLANGIÇ AYARLARI ---
+ADMIN_PASSWORD = "123"
+# ... kodun geri kalanı ...
